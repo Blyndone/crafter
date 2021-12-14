c36e4b2c34878ad0fb180a79046da2d6085baf0e
Rick Duel <Rickduel@gmail.com>
2021-12-14 08:15:54 -0500

    messy function testing

--- scripts/crafter.js
+++ scripts/crafter.js
@@ -1,5 +1,5 @@
 console.log("Crafter || Active");
-class Crafter{
+class Crafter {
     static ID = 'crafter';
     static TEMPLATES = {
         CRAFTER: `modules/${this.ID}/templates/crafter.hbs`
@@ -15,52 +15,148 @@ class Crafter{
 
 
 
-/**
- * A single Recipe in our list of Recipes.
- * @typedef {Object} Recipe
- * @property {string} id - A unique ID to identify this recipe.
- * @property {string} label - The text of the name of the recipe.
- * @property {string} professionType - The profession that crafts this recipe
- * @property {Object} Component - an object that contains what kind of materials can be used
- * @property {string} craftingTime - time required to craft item
- * @property {string} difficulty - Either a check needed or a profeciency needed
- */
+    /**
+     * A single Recipe in our list of Recipes.
+     * @typedef {Object} Recipe
+     * @property {string} id - A unique ID to identify this recipe.
+     * @property {string} label - The text of the name of the recipe.
+     * @property {string} professionType - The profession that crafts this recipe
+     * @property {Object} Component - an object that contains what kind of materials can be used
+     * @property {string} craftingTime - time required to craft item
+     * @property {string} difficulty - Either a check needed or a profeciency needed
+     */
 
-/**
- * A single Component used in a recipe.
- * @typedef {Object} Component
- * @property {string} id - A unique ID to identify this component.
- * @property {string} label - The text of the name of the component.
- * @property {string} keyType - keyword that designates the type of material
- * @property {int} value - the cost of the component in GP
- * @property {int} quantity - the number of items to satisfy the recipe 
- */
+    /**
+     * A single Component used in a recipe.
+     * @typedef {Object} Component
+     * @property {string} id - A unique ID to identify this component.
+     * @property {string} label - The text of the name of the component.
+     * @property {string} keyword - keyword that designates the type of material
+     * @property {int} value - the cost of the component in GP
+     * @property {int} quantity - the number of items to satisfy the recipe 
+     */
 
 
 }
 
-Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
+Hooks.once('devModeReady', ({
+    registerPackageDebugFlag
+}) => {
     registerPackageDebugFlag(Crafter.ID);
-  });
+});
 
 
-  class RecipeData{
-    
+class RecipeData {
 
-    static descriptionFromName(name){
+        //returns the description of an item by searching for an exact name
+    static descriptionFromName(name) {
 
-    Crafter.log(false, game.items.getName(name).data.data.description.value);
-    
-}
-    static searchDescription(term){
-    Crafter.log(false, game.items.find(item =>item.data.data.description.value === term).data.name);
+        return game.items.getName(name).data.data.description.value;
+
     }
+    //returns the name of an item by searching for the exact description
+    static searchDescription(term) {
+        
+        return game.items.find(item => item.data.data.description.value === term).data.name;
+    }
+    //returns an array of item keys based 
+    static findComponents(book, term) {  
+        let bookObj = game.actors.getName(book);
+        let multiObj = bookObj.data.items.filter(item => (item.data.data.description.value).includes(term) );
+        let multi = [];
+        for (let i = 0; i< multiObj.length; i++){
+            multi.push(multiObj[i].data._id)
+        }
+        return  multi;
+    }
+    // creates a Component Object from an item key
+    static createComponent (actor, key){
+        let item = actor.data.items.get(key);
+        let kwStart = (item.data.data.description.value).indexOf("@");
+        let kw = (item.data.data.description.value).slice(kwStart+1, kwStart+5);
 
-    static searchDescriptionMulti(term){
-        Crafter.log(false, game.items.filter(item =>item.data.data.description.value === term).slice(0));
+        const newComponent = {
+            id: foundry.utils.randomID(16),
+            label: item.data.name,
+            keyword: kw,
+            value: item.data.data.price,
+            quantity: 1
         }
 
+            return newComponent;
+    }
+        //creates a recipe Item on Book actor with a component?
+    static createRecipe(book, componentKey, targetItem){
+        let prof = "Blacksmithing";
+        let cTime = 1;
+        let diff = "Hard";
+
+        const recipe = {
+            id: foundry.utils.randomID(16),
+            label: targetItem,
+            professionType: prof,
+            Component: this.createComponent(book,componentKey),
+            craftingTime: cTime,
+            difficulty: diff
+        }
+
+        
+        const recipeData = {
+            _id: recipe.id,
+            name: recipe.label,
+            type: "loot",
+            folder: null
+        }
+
+        const tempItem = new Item( 
+            {
+              name: recipe.label,
+              type: "loot",
+              data: {
+
+                description: {
+                    value: "@"+recipe.Component.keyword
+                }
+                  
+
+              },
+              img: "icons/sundries/books/book-clasp-spiral-green.webp",
+
+                              
+            }
+            
+          );
+
+        book.createEmbeddedDocuments("Item", [tempItem.toObject()]); 
+            Crafter.log(false, tempItem)
+        let finishedRecipeItem = recipe;
+        return tempItem;
+    }
+
+    static findRecipes(book, componentKey){
+        book = game.actors.getName(book);
+        let multiObj = book.data.items.filter(item => (item.data.data.description.value).includes(componentKey) );
+        let multi = [];
+        for (let i = 0; i< multiObj.length; i++){
+            multi.push(multiObj[i].data._id)
+        }
+        return  multi;
+        return rec;
 
 
-  }
+    }
+  /*   //finds recipes within a recipe book actor by name and keyword
+    static findBook (name, keyword)  {
+        let book = game.actors.getName(name);
+        let recipe = [this.searchDescriptionMulti(book, keyword)];
+        let allRecipe = [];
+        for (let i=0; i<recipe.length; i++){
+            allRecipe.push(this.createComponent(book, recipe[0][i]));
+        }
+       
+        
+        return  allRecipe;
  
+    }*/
+
+}
