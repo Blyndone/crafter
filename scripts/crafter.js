d4aad9c6a6f2ace592a1af821e3e418855c86b1d
Rick Duel <Rickduel@gmail.com>
2021-12-15 17:07:07 -0500

    Recipe Generation Form mostly works

--- scripts/crafter.js
+++ scripts/crafter.js
@@ -37,6 +37,53 @@ class Crafter {
      * @property {boolean} isComponent - true for compoenents
      */
 
+     static cptHook(){
+        CRAFTER.LOG(false, "Hooked");
+        }
+
+    static initialize() {
+         this.recipeMenu = new RecipeMenu();
+         }
+
+
+
+
+    static renderJournalDirectory(app, html)
+   {
+        const button = $(`<button class="crafter-button">Crafing Menu</button>`);
+
+         let footer = html.find('.directory-footer');
+         if (footer.length === 0)
+         {
+            footer = $(`<footer class="directory-footer"></footer>`);
+            html.append(footer);
+         }
+         footer.append(button);
+
+          button.click(() =>
+         {  
+            Crafter.recipeMenu.render(true);
+         }); 
+      
+
+   /*    if (!(game.user.isGM && game.settings.get(constants.moduleName, settings.showFolder)))
+      {
+         const folder = Utils.getQuestFolder();
+         if (folder !== void 0)
+         {
+            const element = html.find(`.folder[data-folder-id="${folder.id}"]`);
+            if (element !== void 0)
+            {
+               element.remove();
+            }
+         }
+      } */
+   }   
+
+
+
+
+
 
 }
 
@@ -45,6 +92,16 @@ Hooks.once('devModeReady', ({
 }) => {
     registerPackageDebugFlag(Crafter.ID);
 });
+
+Hooks.on('renderJournalDirectory', Crafter.renderJournalDirectory);
+
+
+
+  Hooks.once('init', () => {
+    Crafter.initialize();
+  });
+
+
 
 
 
@@ -74,7 +131,7 @@ async function createRecipe(book, targetItem){
 
 
 class RecipeData {
-
+   
 
         //returns the description of an item by searching for an exact name
     static descriptionFromName(name) {
@@ -82,10 +139,16 @@ class RecipeData {
         return game.items.getName(name).data.data.description.value;
 
     }
+
+
     //returns the name of an item by searching for the exact description
     static searchDescription(term) {
         
         return game.items.find(item => item.data.data.description.value === term).data.name;
+    }
+    static itemFromName(name) {
+        
+        return game.items.getName(name);
     }
     //returns an array of item keys based 
     static findComponents(book, term) {  
@@ -131,7 +194,7 @@ class RecipeData {
         let build = [];
         build.push("<br>@")
         for (let i = 0; i < [...component].length; i++) {
-            build.push(args[i]+"<br>@");            
+            build.push(build[i]+"<br>@");            
         }
         build.push(professionType+"<br>@");
         build.push(craftingTime+"<br>@");
@@ -149,10 +212,6 @@ class RecipeData {
 
     }
 
-
-
-
-
   /*   //finds recipes within a recipe book actor by name and keyword
     static findBook (name, keyword)  {
         let book = game.actors.getName(name);
@@ -166,5 +225,120 @@ class RecipeData {
         return  allRecipe;
  
     }*/
+
+}
+class CraftingMenu extends FormApplication{
+    
+}
+
+class RecipeMenu extends FormApplication {
+    static get defaultOptions() {
+        const defaults = super.defaultOptions;
+
+        const overrides = {
+            closeOnSubmit: false,
+            height: '700',
+            width: '500',
+            id: 'crafter',
+            submitOnChange: true,
+            template: Crafter.TEMPLATES.CRAFTER,
+            title: 'Recipe Menu',
+            currentItem: 'Potion of Healing',
+
+        };
+
+        const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
+
+        return mergedOptions;
+
+    }
+    getData(options) {
+    //    if (this.currentItem === undefined){
+   //         return RecipeData.itemFromName("Potion of Healing")
+    //    }else
+        return {
+            item: RecipeData.itemFromName(this.currentItem)
+        }
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+        html.on('click', "[data-action]", this._handleButtonClick.bind(this));
+      }
+    
+
+    async _updateObject(event, formData) {
+        if (event.type === "submit"){
+        const caller = event.submitter.value;
+        switch (caller) {
+            case 'changeItem': {
+                this.currentItem = formData.submitText;
+                this.render();
+                break;
+            }
+            case 'createRecipe': {
+            Crafter.log(false,"Creating Recipe", formData)
+            if (formData.bookName && this.currentItem){
+                Crafter.log(false, "Crafting is Ready");
+                createRecipe(formData.bookName, this.currentItem);
+            }else{
+                Crafter.log(false, "Crafting Failed",formData.bookName, this.currentItem.name)
+            }
+
+
+            
+
+
+
+
+                break;
+            }
+            default:
+        }
+        }
+        // console.log(formData);
+        // console.log("Form Data ^");
+        // console.log(event);
+
+       
+
+        }
+
+    async _handleButtonClick (event){
+        const clickedElement = $(event.currentTarget);
+        const action = clickedElement.data().action;
+        const toDoId = clickedElement.parents('[data-todo-id]')?.data()?.todoId;
+        if(action){
+        
+        
+        switch(action){
+            case 'create': {
+                await Crafter.log(false, 'Create',action)
+                this.currentItem = "Healing Herbs";
+                this.render();
+                break;
+            }
+            case 'delete':{
+                await Crafter.log(false, 'Delete ',action)
+                this.currentItem = "Dagger";
+                this.render();
+                break;
+            }
+            case 'createRecipe':{
+                createRecipe('[book-name]', '[item.name]');
+                this.render();
+
+            }
+            
+            default :
+                Crafter.log(false, 'Invalid action detected ',action)
+            this.render();
+
+        }
+    }
+
+
+    }  
+
 
 }
